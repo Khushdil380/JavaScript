95a145bfa3ce551e09dbb2a8f005677d5bca6745
Khushdil Ansari <119004708+Khushdil380@users.noreply.github.com>
2023-10-05 20:54:36 +0000

    Variable

---
+++ Basic/L04_Variable.js
@@ -0,0 +1,24 @@
+/* 
+    variable and const
+    variable- jisko change kr skte hai
+    const- isme changes nhi kr skte
+
+    Variable- Used to storing
+*/
+
+const AccountId=780380
+let AccountEmail="khushdilansari345@gmail.com" // Scope
+var AccountPassword="Khush@03"      // var se scope define nhi hota hai
+let AccountCity="Indore"
+
+/* 
+    Investigation
+*/
+// AccoundId=5885   // (Not Allowed)
+
+console.log(AccountId);
+
+AccountEmail="abcd@gmail.com"
+AccountPassword="kajkdh"
+
+console.table([AccountId, AccountEmail,AccountPassword, AccountCity])
